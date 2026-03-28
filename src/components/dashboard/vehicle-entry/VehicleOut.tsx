bd7ebf1736dd6d74920452b043633844b380300a
Sikka IND <sikkalmcg@gmail.com>
2026-03-28 09:54:02 +0000

    assets mai jo images hai wo saari show kyu nhi ho rhi hai poore website

--- src/components/dashboard/vehicle-entry/VehicleOut.tsx
+++ src/components/dashboard/vehicle-entry/VehicleOut.tsx
@@ -10,6 +10,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Separator } from '@/components/ui/separator';
+import { Badge } from '@/components/ui/badge';
 import { 
     Plus, 
     ShieldCheck, 
