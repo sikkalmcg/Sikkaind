d76b1f4fc90b1c0344832d8cd9cfd5b9c25b1b10
Sikka IND <sikkalmcg@gmail.com>
2026-03-31 10:45:36 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: Card is

--- src/components/dashboard/supervisor-task/TaskModal.tsx
+++ src/components/dashboard/supervisor-task/TaskModal.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, TableFooter } from '@/components/ui/table';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Card } from '@/components/ui/card';
 import { useToast } from '@/hooks/use-toast';
 import { 
     ShieldCheck, 
