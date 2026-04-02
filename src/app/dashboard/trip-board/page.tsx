33742689f46b7a365e529b91c01452de14efd81a
Sikka IND <sikkalmcg@gmail.com>
2026-04-02 09:41:38 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: preview

--- src/app/dashboard/trip-board/page.tsx
+++ src/app/dashboard/trip-board/page.tsx
@@ -659,7 +659,7 @@ function TripBoardContent() {
       {viewTripData && <TripViewModal isOpen={!!viewTripData} onClose={() => setViewTripData(null)} trip={viewTripData} />}
       {editVehicleTrip && <EditVehicleModal isOpen={!!editVehicleTrip} onClose={() => setEditVehicleTrip(null)} trip={editVehicleTrip} onSave={handleUpdateVehicle} />}
       {cancelTripData && <CancelTripModal isOpen={!!cancelTripData} onClose={() => setCancelTripData(null)} trip={cancelTripData} onConfirm={handleCancelTrip} />}
-      {lrPreviewData && <LRPrintPreviewModal isOpen={!!previewLr} onClose={() => setPreviewLr(null)} lr={previewLr} />}
+      {lrPreviewData && <LRPrintPreviewModal isOpen={!!lrPreviewData} onClose={() => setLrPreviewData(null)} lr={lrPreviewData} />}
     </div>
   );
 }
