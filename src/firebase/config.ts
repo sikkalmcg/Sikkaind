50d999287eccd06c7657af6094e512352d1f6cab
Sikka IND <sikkalmcg@gmail.com>
2026-04-21 10:09:33 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,5 +5,5 @@ export const firebaseConfig = {
   projectId: "studio-2134942499-abd6c",
   storageBucket: "studio-2134942499-abd6c.firebasestorage.app",
   messagingSenderId: "852873708191",
-  appId: "1:852873708191:web:5e0dc0e8328f77d4d158aa"
+  appId: "1:852873708191:web:6d4d17b1aa4ee474d158aa"
 };
