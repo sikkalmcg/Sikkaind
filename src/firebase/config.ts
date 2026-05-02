136d62561d35937872812b6c5c6db10efc352ef1
Sikka IND <sikkalmcg@gmail.com>
2026-05-02 10:02:42 +0000

    cammand run kro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDkEktvsr9sIkfV2EO2QVYE7ud_AzozoIs",
   authDomain: "studio-2134942499-abd6c.firebaseapp.com",
-  databaseURL: "https://studio-2134942499-abd6c-default-rtdb.firebaseio.com",
+  databaseURL: "https://studio-2134942499-abd6c-default-rtdb.firebaseio.com/",
   projectId: "studio-2134942499-abd6c",
   storageBucket: "studio-2134942499-abd6c.firebasestorage.app",
   messagingSenderId: "852873708191",
